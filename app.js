02117c50913150ed70a890c59f25693f00f10086
hassanyahya400 <hassanyahya400@gmail.com>
2024-06-07 23:12:35 +0000

    Add-Access-Control-Allow-Origin config

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const axiosInstance = require("./apiClient");
 
 const app = express();
 
+// CORS Middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get("*", (req, res) => {
   axiosInstance
     .get(req.url)
